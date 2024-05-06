9e885534b982202dc6d1cb567c123296bca6ca12
DaBoltGod <82177322+daboltgod@users.noreply.github.com>
2024-05-06 01:25:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,32 @@ function gtag() {
 }
 gtag('js', new Date());
 gtag('config', 'G-77ZE0DPN37');
+
+// AdBlocker warnings
+const fundingChoicesScript = document.createElement('script');
+fundingChoicesScript.async = true;
+fundingChoicesScript.src = "https://fundingchoicesmessages.google.com/i/pub-9451736546409463?ers=1";
+fundingChoicesScript.nonce = "kxAH6LY9FSGC95FCmed1LA";
+document.head.appendChild(fundingChoicesScript);
+
+const googlefcPresentScript = document.createElement('script');
+googlefcPresentScript.nonce = "kxAH6LY9FSGC95FCmed1LA";
+googlefcPresentScript.innerHTML = `
+(function() {
+  function signalGooglefcPresent() {
+    if (!window.frames['googlefcPresent']) {
+      if (document.body) {
+        const iframe = document.createElement('iframe');
+        iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
+        iframe.style.display = 'none';
+        iframe.name = 'googlefcPresent';
+        document.body.appendChild(iframe);
+      } else {
+        setTimeout(signalGooglefcPresent, 0);
+      }
+    }
+  }
+  signalGooglefcPresent();
+})();
+`;
+document.head.appendChild(googlefcPresentScript);
